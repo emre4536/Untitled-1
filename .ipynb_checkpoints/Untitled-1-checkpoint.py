b5b7cc2f5e4aaca813687912f74668d371794891
emre4536 <y.eemre94@gmail.com>
2021-03-09 11:56:24 +0100

    main

---
+++ .ipynb_checkpoints/Untitled-1-checkpoint.py
@@ -0,0 +1,20 @@
+print("hello_gitttt")
+print("hello_gitttt")
+print("hello_gitttt")
+print("hello_gitttt")
+print("hello_gitttt")
+
+
+
+
+
+
+#
+# 
+# PySimpleGUI: The Simple Way to Create a GUI With Python
+
+
+print("hello_gitttt")
+print("hello_gitttt")
+print("hello_gitttt")
+
